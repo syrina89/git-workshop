e38600db7b1f9e6fdf8b88dd2768986c392b424b
syrina <sirina.mak@example.com>
2025-02-22 18:00:11 +0100

    Add/contact route

--- index.js
+++ index.js
@@ -1,6 +1,10 @@
 app.get('/about', (req, res) => {
     res.send('About Page');
 });
+app.get('/contact', (req, res) => {
+    res.send('Contact Page');
+});
+
 
 const express = require("express");
 const app = express();
@@ -10,6 +14,7 @@ const PORT = 3000;
 app.get("/", (req, res) => {
     res.send("Bienvenue sur mon serveur Express !");
 });
+
 
 app.listen(PORT, () => {
     console.log(`Serveur en cours d'exécution sur http://localhost:${PORT}`);
